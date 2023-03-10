de868cbc48ba673e6ec2af4f4cd0a0b6e6e4829d
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-10 16:07:52 +0300

    add ChatViewSet with def list

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from core.models import Chat
+from .serializers import ChatSerializer
+
+
+class ChatViewSet(viewsets.GenericViewSet):
+    def list(self, request):
+        if not request.user.is_authenticated:
+            return Response("User unauthorized", status=401)
+
+        queryset = Chat.objects.all().filter(zones__id=request.user.pk)
+        serialized = ChatSerializer(queryset, many=True)
+        return Response(serialized, status=200)
