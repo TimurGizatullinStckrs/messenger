0c061fc46d0e48f050551f6480593b6c07c5df40
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-10 17:59:57 +0300

    add router

--- src/messenger/urls.py
+++ src/messenger/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from api.views import ChatViewSet
+
+router = routers.DefaultRouter()
+router.register(r'chats', ChatViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
