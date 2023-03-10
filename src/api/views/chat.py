0d7800ce2ada45c38e496205d6354dedcbf44c42
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-10 18:45:56 +0300

    rewrite filter on getting chats queryset in list

--- src/api/views/chat.py
+++ src/api/views/chat.py
@@ -10,6 +10,6 @@ class ChatViewSet(viewsets.GenericViewSet):
         if not request.user.is_authenticated:
             return Response("User unauthorized", status=401)
 
-        queryset = Chat.objects.all().filter(zones__id=request.user.pk)
+        queryset = Chat.objects.all().filter(user__contains=request.user.pk)
         serialized = ChatSerializer(queryset, many=True)
         return Response(serialized, status=200)
