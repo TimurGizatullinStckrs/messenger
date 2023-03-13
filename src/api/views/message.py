ee72bd53ecac180ff149548051a1e1ffa7a3014d
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 19:08:12 +0300

    = rewrite list

--- src/api/views/message.py
+++ src/api/views/message.py
@@ -11,20 +11,8 @@ class MessageViewSet(viewsets.GenericViewSet):
     permission_classes = IsAuthenticated
 
     def list(self, request):
-        limit = request.data.get('limit', None)
-        offset = request.data.get('offset', None)
-
-        if limit is None or offset is None:
-            return Response({'Message': 'Missing required params (limit, offset)'}, status=status.HTTP_400_BAD_REQUEST)
-
         queryset = Message.objects.all()
-        serialized = MessageSerializer(queryset, many=True)
+        paginated_queryset = super().paginate_queryset(queryset)
+        serialized = MessageSerializer(paginated_queryset, many=True)
 
         return Response(serialized, status=status.HTTP_200_OK)
-
-    def filter_queryset(self, queryset: QuerySet) -> QuerySet:
-        if self.action == 'list':
-            limit = self.request.data.get('limit')
-            offset = self.request.data.get('offset')
-
-            return super().filter_queryset(queryset)[offset: offset + limit]
