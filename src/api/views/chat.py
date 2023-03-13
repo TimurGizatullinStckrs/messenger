89ef74b07117837d3e0b754d6ff60452d6b37f60
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 16:11:25 +0300

    ! remove filter logic to overrided method

--- src/api/views/chat.py
+++ src/api/views/chat.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django.db.models import QuerySet
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api.serializers import ChatSerializer
@@ -6,33 +8,34 @@ from core.models import Chat
 
 
 class ChatViewSet(viewsets.GenericViewSet):
-    def list(self, request):
-        if not request.user.is_authenticated:
-            return Response("User unauthorized", status=401)
+    permission_classes = IsAuthenticated
 
-        queryset = Chat.objects.all().filter(zones__id=request.user.pk)
+    def list(self, request):
+        queryset = Chat.objects.all()
         serialized = ChatSerializer(queryset, many=True)
-        return Response(serialized, status=200)
+        return Response(serialized, status=status.HTTP_200_OK)
 
     def create(self, request):
-        if not request.user.is_authenticated:
-            return Response("User unauthorized", status=401)
-
         user_id = request.data.get('user_id', None)
 
         if not user_id:
-            return Response({'Message': "Missing required arguments (user_id)"}, 400)
+            return Response({'Message': "Missing required arguments (user_id)"}, status=status.HTTP_400_BAD_REQUEST)
 
-        chats = (
-            Chat.objects.all()
-            .filter(users__contains=[request.user.pk, user_id])
-            .filter(is_dialog=True)
-        )
+        chats = Chat.objects.all()
 
         if chats:
-            return Response({'Message': "Chat already exists"}, 400)
+            return Response({'Message': "Chat already exists"}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = ChatSerializer(data=request.data)
         chat = serializer.save()
 
-        return Response(chat, status=201)
+        return Response(chat, status=status.HTTP_201_CREATED)
+
+    def filter_queryset(self, queryset: QuerySet) -> QuerySet:
+        if self.action == 'create':
+            return (
+                super().filter_queryset(queryset)
+                .filter(users__cantains=[self.request.user.pk, self.request.data.get('user_id')])
+                .filter(is_dialog=True))
+        elif self.action == 'list':
+            return super().filter_queryset(queryset).filter(user=self.request.user)
