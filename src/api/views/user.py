84993902414dfe1afd5f342b557b0aa668f13773
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 17:36:34 +0300

    = add condition for validating data

--- src/api/views/user.py
+++ src/api/views/user.py
@@ -1,12 +1,13 @@
 from django.db.models import QuerySet
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-
+from rest_framework.permissions import AllowAny
 from api.serializers.user import UserSerializer
 from core.models import User
 
 
 class AuthViewSet(viewsets.GenericViewSet):
+    permission_classes = AllowAny
 
     def sign_in(self, request):
         email = request.data.get('email', None)
@@ -15,3 +16,10 @@ class AuthViewSet(viewsets.GenericViewSet):
         if email is None or password is None:
             Response({'Message': "Missing required params"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
+        if User.objects.all():
+            Response({'Message', "User with such email already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = UserSerializer(data=request.data)
+        user = serializer.save()
+
+        return Response(user, status=status.HTTP_201_CREATED)
