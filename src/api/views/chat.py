ea6e4c003a7707eed978bf82aefa11981af2738c
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 11:43:39 +0300

    rewrite auth checking

--- src/api/views/chat.py
+++ src/api/views/chat.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api.serializers import ChatSerializer
@@ -6,10 +7,9 @@ from core.models import Chat
 
 
 class ChatViewSet(viewsets.GenericViewSet):
-    def list(self, request):
-        if not request.user.is_authenticated:
-            return Response("User unauthorized", status=status.HTTP_401_UNAUTHORIZED)
+    permission_classes = IsAuthenticated
 
+    def list(self, request):
         queryset = Chat.objects.all().filter(user__contains=request.user.pk)
         serialized = ChatSerializer(queryset, many=True)
         return Response(serialized, status=status.HTTP_200_OK)
