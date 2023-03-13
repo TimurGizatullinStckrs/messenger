a501e3b17f217b9c98c989077b97be21466a6da6
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 11:38:09 +0300

    rewrite response status codes

--- src/api/views/chat.py
+++ src/api/views/chat.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from api.serializers import ChatSerializer
@@ -8,8 +8,8 @@ from core.models import Chat
 class ChatViewSet(viewsets.GenericViewSet):
     def list(self, request):
         if not request.user.is_authenticated:
-            return Response("User unauthorized", status=401)
+            return Response("User unauthorized", status=status.HTTP_401_UNAUTHORIZED)
 
         queryset = Chat.objects.all().filter(user__contains=request.user.pk)
         serialized = ChatSerializer(queryset, many=True)
-        return Response(serialized, status=200)
+        return Response(serialized, status=status.HTTP_200_OK)
