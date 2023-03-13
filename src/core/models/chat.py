b93acd380414b2f57d14d165a6a9b51962e04d2c
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 12:51:01 +0300

    update related names

--- src/core/models/chat.py
+++ src/core/models/chat.py
@@ -4,7 +4,7 @@ from django.db import models
 class Chat(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания чата")
     updated_at = models.DateTimeField(auto_now=True, null=True, verbose_name="Время последнего изменения чата")
-    users = models.ManyToManyField('User', related_name="users", verbose_name="Участники чата")
-    last_message = models.ForeignKey('Message', related_name="last_message",
-                                     on_delete=models.PROTECT, verbose_name="Последнее сообщение чата")  # on_delete=models.SET(get_last_msg())??
+    users = models.ManyToManyField('User', related_name="chats", verbose_name="Участники чата")
+    last_message = models.ForeignKey('Message', related_name="last",
+                                     on_delete=models.PROTECT, verbose_name="Последнее сообщение чата")
     is_dialog = models.BooleanField(default=True, verbose_name="Флаг, является ли чат диалогом")
