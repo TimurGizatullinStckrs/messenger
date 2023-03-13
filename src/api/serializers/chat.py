1a7102df92127545b891dabf710d82cdd37b0371
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 11:46:14 +0300

    update chat serializer get pk instead of id now, remove id rules

--- src/api/serializers/chat.py
+++ src/api/serializers/chat.py
@@ -4,11 +4,10 @@ from core.models import Chat
 
 
 class ChatSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
     users = serializers.ManyRelatedField()
     last_message = serializers.CharField()
 
     class Meta:
         model = Chat
-        fields = ['id', 'users', 'last_message']
+        fields = ['pk', 'users', 'last_message']
         depth = 1
