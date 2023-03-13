539f8213fac2457dd1b3098bb63d1641df662e40
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 19:22:36 +0300

    + UserViewSet

---
+++ src/api/views/user.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import get_object_or_404
+
+from api.serializers.user import UserSerializer
+from core.models import User
+
+
+class UserViewSet(viewsets.GenericViewSet):
+    permission_classes = IsAuthenticated
+
+    def retrieve(self, request):
+        queryset = User.objects.all()
+        user_object = get_object_or_404(queryset)
+        serialized = UserSerializer(user_object)
+
+        return Response(serialized, status=status.HTTP_200_OK)
