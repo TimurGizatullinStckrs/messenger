1761075b82e8707f7b32f49d14a3c30d83c8882f
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 19:25:54 +0300

    + filter to get_object_or_404

--- src/api/views/user.py
+++ src/api/views/user.py
@@ -10,9 +10,10 @@ from core.models import User
 class UserViewSet(viewsets.GenericViewSet):
     permission_classes = IsAuthenticated
 
-    def retrieve(self, request):
+    def retrieve(self, request, email: str):
         queryset = User.objects.all()
-        user_object = get_object_or_404(queryset)
+        user_object = get_object_or_404(queryset, email__contains=email)
         serialized = UserSerializer(user_object)
 
         return Response(serialized, status=status.HTTP_200_OK)
+
