57588b10ad4ac216b9b70d3ef34f75bae8f8d116
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 12:28:50 +0300

    update user model and add UserManager

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,14 +1,47 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
-    email = models.CharField(max_length=255, unique=True, verbose_name="Почта пользователя")
-    password = models.CharField(max_length=255, verbose_name="Пароль пользователя")
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(max_length=255, unique=True, verbose_name="Почта пользователя")
     registered_at = models.DateTimeField(auto_now_add=True, verbose_name="Время регистрации")
     updated_at = models.DateTimeField(auto_now=True, null=True, verbose_name="Время последнего изменения профиля")
-    name = models.CharField(max_length=255, null=True, verbose_name="Имя пользователя")
-    surname = models.CharField(max_length=255, null=True, verbose_name="Фамилия пользователя")
     profile_picture = models.ImageField(verbose_name="Фотография профиля")
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
 
     def __str__(self):
         return self.email
