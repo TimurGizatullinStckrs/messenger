8288ee5f5f9b960854e37773907086f1673e8b03
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 15:14:01 +0300

    ! rename custom field

--- src/api/serializers/chat.py
+++ src/api/serializers/chat.py
@@ -5,12 +5,12 @@ from core.models import Chat, Message
 
 class ChatSerializer(serializers.ModelSerializer):
     users = serializers.ManyRelatedField()
-    penultimate_message = serializers.SerializerMethodField()
+    last_message = serializers.SerializerMethodField()
 
     class Meta:
         model = Chat
         fields = ['pk', 'users', 'last_message']
         depth = 1
 
-    def get_penultimate_message(self) -> Message:
+    def get_last_message(self) -> Message:
         return Message.objects.filter(chat=self.pk)[-1]
