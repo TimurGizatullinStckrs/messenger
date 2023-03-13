eaede44e81a4f36a094ecf3b1ad373f793b93ecc
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 14:01:57 +0300

    add field to serializator

--- src/api/serializers/chat.py
+++ src/api/serializers/chat.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 
-from core.models import Chat
+from core.models import Chat, Message
 
 
 class ChatSerializer(serializers.ModelSerializer):
     users = serializers.ManyRelatedField()
-    last_message = serializers.CharField()
+    penultimate_message = serializers.SerializerMethodField()
 
     class Meta:
         model = Chat
         fields = ['pk', 'users', 'last_message']
         depth = 1
+
+    def get_penultimate_message(self) -> Message:
+        return Message.objects.filter(chat=self.pk)[-1]
--- src/core/models/message.py
+++ src/core/models/message.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from api.serializers import ChatSerializer
 from core.models.mixins import DateMixin
 
 
@@ -9,7 +10,7 @@ class Message(models.Model, DateMixin):
                              verbose_name="Чат, содержащий данное сообщение")
     replied_to = models.ForeignKey('Message', on_delete=models.SET_NULL, related_name="replied_messages", null=True,
                                    verbose_name="Сообщение на которое ответили")
-    forwarded_by = models.ForeignKey('User', on_delete=models.SET_NULL, related_name="forwarded_messages", null=True,
+    forwarded_by = models.ForeignKey('User', on_delete=models.DO_NOTHING, related_name="forwarded_messages", null=True,
                                      verbose_name="Кем сообщение было переслано")
     text = models.CharField(max_length=255, null=True, verbose_name="Текст сообщения")
     picture = models.ImageField(null=True, verbose_name="Картинка сообщения")
