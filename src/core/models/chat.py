51b8fd68c99124b5bff5af212743ebb77e3b4000
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-13 12:56:15 +0300

    update on_delete models property

--- src/core/models/chat.py
+++ src/core/models/chat.py
@@ -6,5 +6,5 @@ class Chat(models.Model):
     updated_at = models.DateTimeField(auto_now=True, null=True, verbose_name="Время последнего изменения чата")
     users = models.ManyToManyField('User', related_name="chats", verbose_name="Участники чата")
     last_message = models.ForeignKey('Message', related_name="last",
-                                     on_delete=models.PROTECT, verbose_name="Последнее сообщение чата")
+                                     on_delete=models.SET_NULL, verbose_name="Последнее сообщение чата")
     is_dialog = models.BooleanField(default=True, verbose_name="Флаг, является ли чат диалогом")
--- src/core/models/message.py
+++ src/core/models/message.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Message(models.Model):
-    author = models.ForeignKey('User', on_delete=models.PROTECT, related_name="messages", verbose_name="Отправитель")
-    chat = models.ForeignKey('Chat', on_delete=models.PROTECT, related_name="history",
+    author = models.ForeignKey('User', on_delete=models.SET_NULL, related_name="messages", verbose_name="Отправитель")
+    chat = models.ForeignKey('Chat', on_delete=models.CASCADE, related_name="history",
                              verbose_name="Чат, содержащий данное сообщение")
-    replied_to = models.ForeignKey('Message', on_delete=models.PROTECT, related_name="replied_messages", null=True,
+    replied_to = models.ForeignKey('Message', on_delete=models.SET_NULL, related_name="replied_messages", null=True,
                                    verbose_name="Сообщение на которое ответили")
-    forwarded_by = models.ForeignKey('User', on_delete=models.PROTECT, related_name="forwarded_messages", null=True,
+    forwarded_by = models.ForeignKey('User', on_delete=models.SET_NULL, related_name="forwarded_messages", null=True,
                                      verbose_name="Кем сообщение было переслано")
     text = models.CharField(max_length=255, null=True, verbose_name="Текст сообщения")
     picture = models.ImageField(null=True, verbose_name="Картинка сообщения")
