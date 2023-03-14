ad0ceb8c64be9bf67a0c72783f9579fd9f0502ca
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-14 12:25:33 +0300

    ! update urls

--- src/messenger/urls.py
+++ src/messenger/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 
-from api.views import ChatViewSet
 
-router = routers.DefaultRouter()
-router.register(r'chats', ChatViewSet)
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include("api.urls"))
+]
