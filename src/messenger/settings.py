3166d49a8a521bd23edb5ce694b52072f2a09176
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-14 12:25:25 +0300

    ! remove additional apps to the end

--- src/messenger/settings.py
+++ src/messenger/settings.py
@@ -10,15 +10,15 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'core',
-    'api',
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core',
+    'api',
 ]
 
 MIDDLEWARE = [
