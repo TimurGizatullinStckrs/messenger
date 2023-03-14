8cc71005cf1c4204e1ff333b33d5677addd975cc
TimurGizatullinStckrs <timurgizatullin@stackers.team>
2023-03-14 13:43:14 +0300

    + register User

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from core.models import Message, Chat
+from core.models import Message, Chat, User
 
 admin.site.register(Message)
 admin.site.register(Chat)
+admin.site.register(User)
